d853927d1768ec2d9ab74e9546dda589c4ca1651
qualia-5w4 <geniusnaru@gmail.com>
2023-09-19 23:31:11 +0900

    add initial files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  bracketSameLine: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  semi: true,
+  printWidth: 110,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
