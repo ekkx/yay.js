c31297173f34f2f4acc894f8fea19add5a9d42c7
qualia-5w4 <geniusnaru@gmail.com>
2023-09-20 10:13:08 +0900

    add config values

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const ID_CHECK_SECRET_KEY = '4aa6d1c301a97154bc1098c2';
+export const API_KEY = 'ccd59ee269c01511ba763467045c115779fcae3050238a252f1bd1a4b65cfec6';
+export const SHARED_KEY = 'yayZ1';
+export const SHARED_STORE_KEY = 'yayZ1payment';
+export const API_VERSION_KEY = 'a9551e9986e04937b68dfd4d9e1343e0';
+export const API_VERSION_NAME = '3.24';
+export const VERSION_NAME = '3.24.0';
+
+export const BASE_API_URL = 'https://api.yay.space';
+export const BASE_CASSANDRA_URL = 'https://cas.yay.space';
+export const BASE_CONFIG_URL = 'https://settings.yay.space';
+export const WEB_SOCKET_URL = 'wss://cable.yay.space';
