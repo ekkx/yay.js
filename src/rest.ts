434befde7e2dafb023ece56c97c02d34f79835e2
qualia-5w4 <geniusnaru@gmail.com>
2023-09-23 13:01:33 +0900

    e

--- src/rest.ts
+++ src/rest.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponseHeaders } from 'axios';
 import { v4 as uuid } from 'uuid';
 
-import { RequestMethod } from './lib/types';
-import type { RequestHeaders } from './lib/types';
+import { RequestMethod } from './lib/Types';
+import type { RequestHeaders } from './lib/Types';
 
 /**
  * Represents the class that manages handlers for endpoints
