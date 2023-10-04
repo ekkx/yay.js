68f0503a135af8c66b24ec85eef64b37bf7bda5b
qualia-5w4 <geniusnaru@gmail.com>
2023-10-04 09:45:39 +0900

    add post models

--- src/util/Models.ts
+++ src/util/Models.ts
@@ -59,18 +59,82 @@ export interface User {
 	vip: boolean;
 	vip_until?: number;
 }
-
 export interface GiftingAbility {
 	can_receive: boolean;
 	can_send: boolean;
 	enabled: boolean;
 	user_id: number;
 }
-
 export interface GroupUser {
 	user: User;
 	is_moderator: boolean;
 	pending_transfer: boolean;
 	pending_deputize: boolean;
 	title: string;
+}
+export interface ConferenceCall {}
+export interface Group {}
+export interface Shareable {}
+export interface SharedUrl {}
+export interface Survey {}
+export interface Video {}
+export interface GiftCount {}
+export interface ThreadInfo {}
+export interface MessageTag {}
+export interface Post {
+	id?: number;
+	text?: string;
+	postType?: string;
+	groupId?: number;
+	fontSize?: number;
+	color?: number;
+	likesCount?: number;
+	createdAt?: number;
+	updatedAt?: number;
+	editedAt?: number;
+	liked?: boolean;
+	likers?: User[];
+	tag?: string;
+	likersCount?: number;
+	repostsCount?: number;
+	reposted?: boolean;
+	repostable?: boolean;
+	reportedCount?: number;
+	conversationId?: number;
+	inReplyTo?: number;
+	inReplyToPost?: Post;
+	inReplyToPostCount?: number;
+	user?: User;
+	mentions?: User[];
+	group?: Group;
+	conferenceCall?: ConferenceCall;
+	attachment?: string;
+	attachmentThumbnail?: string;
+	attachment_2?: string;
+	attachment_2Thumbnail?: string;
+	attachment_3?: string;
+	attachment_3Thumbnail?: string;
+	attachment_4?: string;
+	attachment_4Thumbnail?: string;
+	attachment_5?: string;
+	attachment_5Thumbnail?: string;
+	attachment_6?: string;
+	attachment_6Thumbnail?: string;
+	attachment_7?: string;
+	attachment_7Thumbnail?: string;
+	attachment_8?: string;
+	attachment_8Thumbnail?: string;
+	attachment_9?: string;
+	attachment_9Thumbnail?: string;
+	shareable?: Shareable;
+	sharedUrl?: SharedUrl;
+	survey?: Survey;
+	videos?: Video[];
+	giftsCount?: GiftCount[];
+	sharedThread?: ThreadInfo;
+	threadId?: number;
+	thread?: ThreadInfo;
+	highlighted?: boolean;
+	messageTags?: MessageTag[];
+	isFailToSend?: boolean;
 }
--- src/util/Responses.ts
+++ src/util/Responses.ts
@@ -3,14 +3,68 @@ import { User } from './Models';
 export interface BlockedUserIdsResponse {
 	blockIds: number[];
 }
-
 export interface BlockedUsersResponse {
-	blocked_count: number;
-	last_id: number;
+	blockedCount: number;
+	lastId: number;
 	users: User[];
 }
-
-export interface PostResponse {}
+export interface PostResponse {
+	id?: number;
+	text?: string;
+	postType?: string;
+	groupId?: number;
+	fontSize?: number;
+	color?: number;
+	likesCount?: number;
+	createdAt?: number;
+	updatedAt?: number;
+	editedAt?: number;
+	liked?: boolean;
+	likers?: User[];
+	tag?: string;
+	likersCount?: number;
+	repostsCount?: number;
+	reposted?: boolean;
+	repostable?: boolean;
+	reportedCount?: number;
+	conversationId?: number;
+	inReplyTo?: number;
+	inReplyToPost?: Post;
+	inReplyToPostCount?: number;
+	user?: User;
+	mentions?: User[];
+	group?: Group;
+	conferenceCall?: ConferenceCall;
+	attachment?: string;
+	attachmentThumbnail?: string;
+	attachment_2?: string;
+	attachment_2Thumbnail?: string;
+	attachment_3?: string;
+	attachment_3Thumbnail?: string;
+	attachment_4?: string;
+	attachment_4Thumbnail?: string;
+	attachment_5?: string;
+	attachment_5Thumbnail?: string;
+	attachment_6?: string;
+	attachment_6Thumbnail?: string;
+	attachment_7?: string;
+	attachment_7Thumbnail?: string;
+	attachment_8?: string;
+	attachment_8Thumbnail?: string;
+	attachment_9?: string;
+	attachment_9Thumbnail?: string;
+	shareable?: Shareable;
+	sharedUrl?: SharedUrl;
+	survey?: Survey;
+	videos?: Video[];
+	giftsCount?: GiftCount[];
+	sharedThread?: ThreadInfo;
+	threadId?: number;
+	thread?: ThreadInfo;
+	highlighted?: boolean;
+	messageTags?: MessageTag[];
+	isFailToSend?: boolean;
+}
 export interface PostsResponse {}
 export interface BgmsResponse {}
 export interface ConferenceCallResponse {}
