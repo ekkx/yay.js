d3ea0b79fb7954d32149aea4b4d9198d9532cebe
qualia-5w4 <geniusnaru@gmail.com>
2023-10-04 23:18:30 +0900

    add addGroupHighlightPost

--- src/lib/Post.ts
+++ src/lib/Post.ts
@@ -5,11 +5,28 @@ import { RequestMethod } from '../util/Types';
 export class PostAPI {
 	public constructor(private readonly base: BaseClient) {}
 
-	addBookmark = async (userId: number, id: number): Promise<BookmarkPostResponse> => {
+	public addBookmark = async (userId: number, id: number): Promise<BookmarkPostResponse> => {
 		return await this.base.request({
 			method: RequestMethod.PUT,
 			route: `v1/users/${userId}/bookmarks/${id}`,
 			requireAuth: true,
+		});
+	};
+
+	public addGroupHighlightPost = async (groupId: number, postId: number): Promise<BookmarkPostResponse> => {
+		return await this.base.request({
+			method: RequestMethod.PUT,
+			route: `v1/groups/${groupId}/highlights/${postId}`,
+			requireAuth: false,
+		});
+	};
+
+	public createGroupCallPost = async (): Promise<BookmarkPostResponse> => {
+		return await this.base.request({
+			method: RequestMethod.POST,
+			route: `v2/posts/new_conference_call`,
+			requireAuth: false,
+			json: {},
 		});
 	};
 }
