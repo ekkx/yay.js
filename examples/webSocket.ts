4f92faab0f6b6c877da6b4208201cbe35351d53f
qualia-5w4 <geniusnaru@gmail.com>
2023-10-13 21:11:45 +0900

    ブッサイクなところ修正

--- examples/webSocket.ts
+++ examples/webSocket.ts
@@ -1,7 +1,7 @@
 import { Client, GatewayIntents } from 'yay.js';
 
 const client = new Client({
-    // イベントを取得する範囲を指定
+	// イベントを取得する範囲を指定
 	intents: [
         GatewayIntents.ChatMessage,
         GatewayIntents.GroupUpdates
@@ -14,19 +14,20 @@ client.on('ready', () => {
 
 // メッセージのイベントを取得
 client.on('messageCreate', async (message) => {
-    if (message.text === 'ping') {
-        await client.reply({
-            chatRoomId: message.roomId, text: 'pong'
-        });
-    }
+	if (message.text === 'ping') {
+		await client.reply({
+			chatRoomId: message.roomId,
+			text: 'pong',
+		});
+	}
 });
 
 // サークル更新のイベントを取得
 client.on('groupUpdate', async (groupId) => {
-    console.log(groupId)
+	console.log(groupId);
 });
 
 client.login({
 	email: 'yourEmail',
-    password: 'yourPassword',
+	password: 'yourPassword',
 });
