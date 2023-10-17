afa2e0aca24fa906079bb7d24d56cbf62216e533
qualia-5w4 <geniusnaru@gmail.com>
2023-10-17 14:04:31 +0900

    add game api

--- src/lib/Game.ts
+++ src/lib/Game.ts
@@ -1,5 +1,23 @@
+import { Walkthrough } from '../util/Models';
 import { BaseClient } from '../client/BaseClient';
+import { RequestMethod } from '../util/Types';
 
 export class GameAPI {
 	public constructor(private readonly base: BaseClient) {}
+
+	public getWalkthroughs = async (options: { appId: number }): Promise<Walkthrough[]> => {
+		return await this.base.request({
+			method: RequestMethod.GET,
+			route: `v1/games/apps/${options.appId}/walkthroughs`,
+			requireAuth: false,
+		});
+	};
+
+	public requestWalkthrough = async (options: { id: number }) => {
+		return await this.base.request({
+			method: RequestMethod.POST,
+			route: `v1/groups/${options.id}/request_walkthrough`,
+			requireAuth: false,
+		});
+	};
 }
