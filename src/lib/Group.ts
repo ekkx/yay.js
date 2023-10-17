a61c856aa957ae701f0c599f7c7794167ed38588
qualia-5w4 <geniusnaru@gmail.com>
2023-10-17 14:13:03 +0900

    add group api

--- src/lib/Group.ts
+++ src/lib/Group.ts
@@ -1,5 +1,47 @@
+import { RequestMethod } from '../util/Types';
 import { BaseClient } from '../client/BaseClient';
 
 export class GroupAPI {
 	public constructor(private readonly base: BaseClient) {}
+
+	public acceptModeratorOffer = async (options: { id: number }) => {
+		return await this.base.request({
+			method: RequestMethod.PUT,
+			route: `v1/groups/${options.id}/deputize`,
+			requireAuth: false,
+		});
+	};
+
+	public acceptOwnershipOffer = async (options: { id: number }) => {
+		return await this.base.request({
+			method: RequestMethod.PUT,
+			route: `v1/groups/${options.id}/transfer`,
+			requireAuth: false,
+		});
+	};
+
+	public acceptUserRequest = async (options: { id: number; userId: number }) => {
+		return await this.base.request({
+			method: RequestMethod.POST,
+			route: `v1/groups/${options.id}/accept/${options.userId}`,
+			requireAuth: false,
+		});
+	};
+
+	public addRelatedGroups = async (options: { id: number; relatedGroupId: number }) => {
+		return await this.base.request({
+			method: RequestMethod.PUT,
+			route: `v1/groups/v${options.id}/related`,
+			params: { 'related_group_id[]': options.relatedGroupId },
+			requireAuth: false,
+		});
+	};
+
+	public banUser = async (options: { id: number; userId: number }) => {
+		return await this.base.request({
+			method: RequestMethod.POST,
+			route: `v1/groups/${options.id}/ban/${options.userId}`,
+			requireAuth: false,
+		});
+	};
 }
