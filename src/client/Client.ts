4a03c7371182e590a904741eefc7180d1f4c4d1b
qualia-5w4 <geniusnaru@gmail.com>
2023-10-18 11:06:17 +0900

    typedocの設定を追加

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -35,18 +35,6 @@ import { Post, SharedUrl, Walkthrough } from '../util/Models';
 import { objectToSnake } from '../util/CaseConverter';
 
 export class Client extends BaseClient {
-	/**
-	 * Returns the average of two numbers.
-	 *
-	 * @remarks
-	 * This method is part of the {@link core-library#Statistics | Statistics subsystem}.
-	 *
-	 * @param x - The first input number
-	 * @param y - The second input number
-	 * @returns The arithmetic mean of `x` and `y`
-	 *
-	 * @beta
-	 */
 	public constructor(options?: ClientOptions) {
 		super(options);
 	}
