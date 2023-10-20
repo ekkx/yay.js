d8ae1a955a85ef2ed2418762301d79b929bd3179
qualia-5w4 <geniusnaru@gmail.com>
2023-10-20 20:06:10 +0900

    add mutekeyword api endpoints

--- src/lib/MuteKeyword.ts
+++ src/lib/MuteKeyword.ts
@@ -1,4 +1,6 @@
+import { HttpMethod } from '../util/Types';
 import { BaseClient } from '../client/BaseClient';
+import { CreateMuteKeywordResponse, MuteKeywordResponse } from '../util/Responses';
 
 /**
  * ミュートキーワードAPI
@@ -11,4 +13,43 @@ import { BaseClient } from '../client/BaseClient';
  */
 export class MuteKeywordAPI {
 	public constructor(private readonly base: BaseClient) {}
+
+	public createKeyword = async (options: { word: string; context: string[] }): Promise<CreateMuteKeywordResponse> => {
+		return await this.base.request({
+			method: HttpMethod.POST,
+			route: `v1/hidden/words`,
+			requireAuth: false,
+			json: { word: options.word, context: options.context },
+		});
+	};
+
+	public deleteKeyword = async (options: { keywordIds: number[] }) => {
+		return await this.base.request({
+			method: HttpMethod.DELETE,
+			route: `v1/hidden/words`,
+			requireAuth: false,
+			params: { 'ids[]': options.keywordIds },
+		});
+	};
+
+	public getKeywords = async (): Promise<MuteKeywordResponse> => {
+		return await this.base.request({
+			method: HttpMethod.GET,
+			route: `v1/hidden/words`,
+			requireAuth: false,
+		});
+	};
+
+	public updateKeyword = async (options: {
+		keywordId: number;
+		word: string;
+		context: string[];
+	}): Promise<CreateMuteKeywordResponse> => {
+		return await this.base.request({
+			method: HttpMethod.PUT,
+			route: `v1/hidden/words/${options.keywordId}`,
+			requireAuth: false,
+			json: { word: options.word, context: options.context },
+		});
+	};
 }
