4876e3bf1fbcc0f0030d0dcd3a3ee1e6dfe13609
qualia-5w4 <geniusnaru@gmail.com>
2023-10-20 20:18:04 +0900

    add notification api endpoints

--- src/lib/Notification.ts
+++ src/lib/Notification.ts
@@ -1,4 +1,7 @@
+import { HttpMethod } from '../util/Types';
 import { BaseClient } from '../client/BaseClient';
+import { ActivitiesResponse } from '../util/Responses';
+import { BASE_CASSANDRA_URL } from '../util/Constants';
 
 /**
  * 通知API
@@ -11,4 +14,46 @@ import { BaseClient } from '../client/BaseClient';
  */
 export class NotificationAPI {
 	public constructor(private readonly base: BaseClient) {}
+
+	public getUserActivities = async (options: {
+		important?: boolean;
+		fromTimestamp?: number;
+		number?: number;
+	}): Promise<ActivitiesResponse> => {
+		return await this.base.request({
+			method: HttpMethod.GET,
+			route: `api/user_activities`,
+			params: { important: options.important, from_timestamp: options.fromTimestamp, number: options.number },
+			baseURL: BASE_CASSANDRA_URL,
+			requireAuth: false,
+		});
+	};
+
+	public getUserMergedActivities = async (options: { fromTimestamp?: number }): Promise<ActivitiesResponse> => {
+		return await this.base.request({
+			method: HttpMethod.GET,
+			route: `api/v2/user_activities`,
+			params: { from_timestamp: options.fromTimestamp },
+			baseURL: BASE_CASSANDRA_URL,
+			requireAuth: false,
+		});
+	};
+
+	public receivedNotification = async (options: {
+		pid: string;
+		type: string;
+		openedAt?: number;
+	}): Promise<ActivitiesResponse> => {
+		return await this.base.request({
+			method: HttpMethod.POST,
+			route: `api/received_push_notifications`,
+			json: {
+				pid: options.pid,
+				type: options.type,
+				opened_at: options.openedAt,
+			},
+			baseURL: BASE_CASSANDRA_URL,
+			requireAuth: false,
+		});
+	};
 }
