8e509e1fbf7e8e7e91f5cb7007d0355e1743f11d
qualia-5w4 <geniusnaru@gmail.com>
2023-12-31 17:01:52 +0900

    export models and reponses from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './client/Client';
 export * from './util/Events';
 export * from './util/Errors';
+export * from './util/Models';
+export * from './util/Responses';
 export { GatewayIntents } from './util/Types';
 export { mention } from './util/Utils';
