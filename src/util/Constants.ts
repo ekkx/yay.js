a1b9c9de12f61d5499b90a104d14b8c48f88fe22
qualia-5w4 <geniusnaru@gmail.com>
2024-02-07 23:31:57 +0900

    Yay! 3.31.0に対応

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -2,9 +2,9 @@ export const ID_CHECK_SECRET_KEY = '4aa6d1c301a97154bc1098c2';
 export const API_KEY = 'ccd59ee269c01511ba763467045c115779fcae3050238a252f1bd1a4b65cfec6';
 export const SHARED_KEY = 'yayZ1';
 export const SHARED_STORE_KEY = 'yayZ1payment';
-export const API_VERSION_KEY = 'e463476778d84e9ba594a5aaf391a516';
-export const API_VERSION_NAME = '3.30';
-export const VERSION_NAME = '3.30.0';
+export const API_VERSION_KEY = 'e84d8fb180cf420abbfb380100230009';
+export const API_VERSION_NAME = '3.31';
+export const VERSION_NAME = '3.31.0';
 
 export const BASE_HOST = 'api.yay.space';
 export const BASE_API_URL = `https://${BASE_HOST}/`;
